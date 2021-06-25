2ec9f347d75fe0852e08f4483e074756b02a0aad
Barnabas27 <bnyambura032@gmail.com>
2021-06-25 14:04:59 +0300

    add modifications on 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
+#include "holberton.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
-* main - Positive or negative
-* Description: tells which number positive, negative or equal to zero
-* Return: return 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-/*n = rand() - RAND_MAX / 2;*/
-	n = 0;
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
