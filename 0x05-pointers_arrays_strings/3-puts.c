c010652606ed075be7e24f12a1e8eb101b2f648e
Barnabas27 <bnyambura032@gmail.com>
2021-06-28 21:45:19 +0300

    add a unction that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <string.h>
+
+/**
+ *_puts - prints a string followed by a new line
+ *
+ *@str: the string to be printed
+ *
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+
+	_putchar(*str++);
+	_putchar('\n');
+}
