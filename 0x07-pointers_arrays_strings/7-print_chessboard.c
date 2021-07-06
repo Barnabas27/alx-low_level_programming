4306aab13f5d88c627874825b147b2f044e08791
Barnabas27 <bnyambura032@gmail.com>
2021-07-06 23:43:50 +0300

    add function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "holberton.h"
+
+/**
+ *print_chessboard - function that prints the chessboard
+ *@a: number of rows
+ *Return: always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar (*(*(a + i) + j));
+		}
+
+
+	 _putchar ('\n');
+	}
+
+}
