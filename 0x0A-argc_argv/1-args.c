1afcdf9665979f33316ec9d5d85262a7cd5db2f1
Barnabas27 <bnyambura032@gmail.com>
2021-07-12 17:02:29 +0300

    add betty compliance

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,12 +9,12 @@
  *Return: Always 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 	(void)argv;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%d\n", i);
 	}
